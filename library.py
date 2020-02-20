1482ea1524339829f645140cce40725ce2b9b404
Wilson <wilson.jose.lourenco.pereira@gmail.com>
2020-02-20 19:33:59 +0000

    finish send books

--- library.py
+++ library.py
@@ -6,35 +6,43 @@ class Library:
         self.nSignupDays = nSignupDays
         self.nBooksPerSecond = nBooksPerDay
         self.books = books
+        self.booksSent = []
 
     def sumBookValues(self):
         sum = 0
         for i in self.books:
-            sum += main.books[i]
+            sum += main.bookValues[i]
 
     def heuristic(self):
         return self.sumBookValues() / self.nSignupDays * self.nBooksPerSecond
-<<<<<<< HEAD
 
     def biggerThanMinor(self, value):
-        minorValue = books
-        minorIndex = -1
+        minorValue = main.bookValues[self.books[0]]
+        minorIndex = 0
         
-        for 
+        for i in range(1, nBooks):
+            if main.bookValues[self.books[i]] < minorValue:
+                minorValue =  main.bookValues[self.books[i]]
+                minorIndex = i
 
-
-
+        if minorValue < value:
+            return minorIndex
+        
         return -1
 
     def sendBooks(self):
         booksToSend = []
 
-        for i in range(nBooksPerSecond):
+        for i in range(min(len(self.books), nBooksPerSecond)):
                 booksToSend += [i]
         
-        for i in range(end, nBooks):
-            indexToReplace = biggerThanMinor(i, booksToSend)
+        for i in range(min(len(self.books), nBooksPerSecond), len(self.books)):
+            indexToReplace = biggerThanMinor(i, booksToSend) #returns the index of the minor value if smaller then the value of the current i, -1 otherwise
             if indexToReplace != -1:
                 booksToSend[indexToReplace] = i
-=======
->>>>>>> 12a60fafaffc69051f13fccf2116e0334d528f21
+        
+        for i in booksToSend: #So we don't send the same book again
+            main.bookValues[i] = 0
+
+        self.sentBooks += booksToSend
+
